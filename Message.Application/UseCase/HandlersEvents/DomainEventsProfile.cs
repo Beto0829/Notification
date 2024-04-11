574093f73aa12de850b2ab5296072fb456d26994
Beto0829 <todoslospaises123@gmail.com>
2024-04-11 18:13:27 -0500

    Primer commit

---
+++ Message.Application/UseCase/HandlersEvents/DomainEventsProfile.cs
@@ -0,0 +1,13 @@
+﻿using Message.Domain.Entities;
+using Reservar.Common.Domain.Entities.Subscription;
+
+namespace Message.Application.UseCase.HandlersEvents;
+
+public class DomainEventsProfile:Profile
+{
+    public DomainEventsProfile()
+    {
+        CreateMap<EmailCommand, Email>()
+            .ReverseMap();
+    }
+}
---
+++ Message.Application/UseCase/HandlersEvents/Email/EmailHandler.cs
@@ -0,0 +1,26 @@
+using Common.Communication.Consumer.Handler;
+using Message.Infrastructure.Adapter;
+using Common.Communication.Messages;
+using Message.Domain.Entities;
+using Reservar.Common.Domain.Entities.Subscription;
+
+namespace Message.Application.UseCase.HandlersEvents;
+
+public class EmailHandler : IIntegrationMessageHandler<EmailCommand>
+{
+    private readonly IEmailService _emailServices;
+    private readonly IMapper _mapper;
+
+    public EmailHandler(IEmailService emailServices, IMapper mapper)
+    {
+        _emailServices = emailServices ?? throw new ArgumentNullException(nameof(emailServices));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public async Task Handle(IntegrationMessage<EmailCommand> request, CancellationToken cancelToken = default)
+    {
+        _ = request ?? throw new ArgumentNullException(nameof(request.Content),
+            "request object needed to handle this task");
+        await _emailServices.EnviarEmail(_mapper.Map<Email>(request.Content));
+    }
+}
